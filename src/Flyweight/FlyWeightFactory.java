1be99b9fdf17bac9fc12fe330a250db2f6213b91
Pedro de Jesús <Pedro de Jess@DESKTOP-44R2E1V>
2019-04-08 13:39:01 -0500

    Flyweight

---
+++ src/Flyweight/FlyWeightFactory.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Flyweight;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Valentina
+ */
+public class FlyWeightFactory {
+    private ArrayList<FlyWeight> Fly = new ArrayList<>();
+
+    public FlyWeightFactory() {
+    }
+
+    public void CrearPagoEfectivo(int id, String nombrePasajero, String nombreConductor, float monto, String otros) {
+        FlyWeight f = new PagoEfectivo(id, nombrePasajero, nombreConductor, monto, otros);
+        this.Fly.add(f);
+    }
+
+    public void CrearPagoTarjeta(int id, String nombrePasajero, String nombreConductor, float monto, String otros) {
+        FlyWeight f = new PagoTarjeta(id, nombrePasajero, nombreConductor, monto, otros);
+        this.Fly.add(f);
+    }
+
+    public String verSaldosFavor(String nombreU) {
+        String favor = "";
+
+        
+        
+        
+        return favor;
+    }
+
+    public String verSaldosPagos(String nombreU) {
+        String pagado = "";
+
+        return pagado;
+    }
+}
