bf7c901dd28151c0eb7e796f2648de7a5650be24
Pedro de Jesús <Pedro de Jess@DESKTOP-44R2E1V>
2019-04-08 13:19:52 -0500

    Proxy

--- src/taller/facade/Facade.java
+++ src/taller/facade/Facade.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package taller.facade;
+
 import AdapterPackage.Adapter;
 import AdapterPackage.Conductor;
 import AdapterPackage.Pasajero;
@@ -17,69 +18,68 @@ import java.util.ArrayList;
  * @author Nikolas
  */
 public class Facade {
-    
+
     PagoGrupo G1 = new PagoGrupo();
     ReservaGrupo G2 = new ReservaGrupo();
     ArrayList<Usuario> componentes = new ArrayList();
-    private static  Facade facade;
-    
+    private static Facade facade;
+
     public Facade() {
-        Usuario user = new Adapter("Hola","123");
+        Usuario user = new Adapter("Hola", "123");
         this.componentes.add(user);
     }
-    
-    public static Facade getFacade(){
-    if(facade==null){
-        facade = new Facade();
+
+    public static Facade getFacade() {
+        if (facade == null) {
+            facade = new Facade();
+        }
+        return facade;
     }
-    return facade;
-    }
-    
-    
+
     public void CrearReserva(String nombre, String id) {
-        Reserva reserva = new Reserva(nombre,id);
-       G2.AñadirGrupito(reserva);
-        
+        Reserva reserva = new Reserva(nombre, id);
+        G2.AñadirGrupito(reserva);
+
     }
 
     public void AñadirReserva(Reserva r) {
         G2.AñadirGrupito(r);
-        
-    }    
-    
+
+    }
+
     public void EliminarReservaGrupo(Reserva r) {
         G2.EliminarAlGrupito(r);
     }
-    
+
     public void ModificarLugarReserva(String id, String Lugar) {
         G2.ModificarLugar(id, Lugar);
     }
 
     public String LeerReserva(String id) {
-       return G2.LeerReserva(id);
+        return G2.LeerReserva(id);
     }
-    
+
     public void CrearPago(String pago, String id) {
         Pago pago1 = new Pago(pago, id);
-        
+
     }
-    
+
     public void AñadirPago(Pago r) {
         G1.AñadirGrupito(r);
     }
-    
+
     public void EliminarPago(Pago r) {
         G1.EliminarAlGrupito(r);
     }
-    
+
     public String LeerPago(String id) {
         return G1.LeerPago(id);
     }
-    
-    public void Modificar_ConceptoPago(String id , String Concepto){
-    G1.ModificarConcepto(id, Concepto);
+
+    public void Modificar_ConceptoPago(String id, String Concepto) {
+        G1.ModificarConcepto(id, Concepto);
     }
-    
+
     public void Acceso(String Accion, Usuario user, String Para) throws NoSuchMethodException {
         for (int i = 0; i < this.componentes.size(); i++) {
             if (user.getUsuario().compareTo(componentes.get(i).getUsuario()) == 0 && user.getPassword().compareTo(componentes.get(i).getPassword()) == 0) {
@@ -144,9 +144,10 @@ public class Facade {
         }
 
     }
+
     public void Crear_Usuario(String User, String pass, String Tipo) {
         if (Tipo.equals("Pasajero")) {
-            Usuario usuario = new Pasajero(User,pass);
+            Usuario usuario = new Pasajero(User, pass);
             usuario.setTipo_Usuario(Tipo);
             componentes.add(usuario);
 
@@ -159,12 +160,25 @@ public class Facade {
             usuario.setTipo_Usuario(Tipo);
             componentes.add(usuario);
 
+        } else {
+            System.out.print("No hizo match tipo");
         }
-        else System.out.print("No hizo match tipo");
-    } 
+    }
 
+    public void crearCredito() {
 
-      
-    
-    
+    }
+
+    public void crearDebito() {
+
+    }
+
+    public void crearTransporteIndividial() {
+
+    }
+
+    public void crearAgrupaciones() {
+
+    }
+
 }
